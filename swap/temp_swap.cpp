9f8c3f54f04be2f86d36f5dbaee387eaa52a3d0c
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-11-03 04:25:45 +0900

    swap

---
+++ swap/temp_swap.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+//inline関数    呼び出し側にコードが展開されるので関数呼び出しに伴うオーバーヘッド削減、特に内容が小さく頻繁に呼ばれる場合に有用
+//テンプレート   広範囲の型に適用できるようにする
+template<typename T>
+inline void swap(T *a,T *b) {
+  T temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+template<typename T>
+inline void show(T a,T b,char x,char y) {
+  std::cout << x << "=" << a << "\t" << y << "=" << b << "\n";
+}
+
+int main(void) {
+  int    a,b;
+  double c,d;
+
+  a = 1; b = 2;
+  show(a,b,'a','b');
+  swap(&a,&b);           //int型と判断
+  show(a,b,'a','b');
+  swap<int>(&a,&b);      //int型を指定
+  show(a,b,'a','b');
+
+  c = 0.5; d = 0.75;
+  std::cout << "\n";
+  show(c,d,'c','d');
+  swap(&c,&d);          //double型と判断
+  show(c,d,'c','d');
+  swap<double>(&c,&d);  //double型を指定
+  show(c,d,'c','d');
+
+
+
+
+  return 0;
+}
