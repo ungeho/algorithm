1d91ad68fdd96caa43935879e0bd0b03d099561e
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-11-04 05:23:56 +0900

    swap

--- swap/pm_swap.cpp
+++ swap/pm_swap.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cassert>
+
+using namespace std;
+
 
 //inline関数    呼び出し側にコードが展開されるので関数呼び出しに伴うオーバーヘッド削減、特に内容が小さく頻繁に呼ばれる場合に有用
 //テンプレート   広範囲の型に適用できるようにする
@@ -11,7 +15,7 @@ inline void swap(T *a,T *b) {
 
 template<typename T>
 inline void show(T a,T b,char x,char y) {
-  std::cout << x << "=" << a << "\t" << y << "=" << b << "\n";
+  cout << x << "=" << a << "\t" << y << "=" << b << "\n";
 }
 
 int main(void) {
@@ -19,22 +23,30 @@ int main(void) {
   double c,d;
 
   a = 1; b = 2;
+
   show(a,b,'a','b');
+
   swap(&a,&b);           //int型と判断
+  assert(a == 2 && b == 1);
   show(a,b,'a','b');
+
   swap<int>(&a,&b);      //int型を指定
+  assert(a == 1 && b == 2);
   show(a,b,'a','b');
+
+  cout << "\n";
 
   c = 0.5; d = 0.75;
-  std::cout << "\n";
+
   show(c,d,'c','d');
+
   swap(&c,&d);          //double型と判断
+  assert(c == 0.75 && d == 0.5);
   show(c,d,'c','d');
+
   swap<double>(&c,&d);  //double型を指定
+  assert(c == 0.5 && d == 0.75);
   show(c,d,'c','d');
-
-
-
 
   return 0;
 }
--- swap/xor_swap.cpp
+++ swap/xor_swap.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
+#include <cassert>
 
-//inline関数    呼び出し側にコードが展開されるので関数呼び出しに伴うオーバーヘッド削減、特に内容が小さく頻繁に呼ばれる場合に有用
-//テンプレート   広範囲の型に適用できるようにする
-template<typename T>
-inline void swap(T *a,T *b) {
+using namespace std;
+
+
+//ビット演算子は浮動小数点型に対応してない為、double型のswapに使おうとするとコンパイラに怒られる。
+inline void swap(int *a,int *b) {
   *b ^= *a; //B XOR A
   *a ^= *b; //A XOR (B XOR A) = (A XOR A) XOR B = B
   *b ^= *a; //(B XOR A) XOR B = (B XOR B) XOR A = A
 }
 
-template<typename T>
-inline void show(T a,T b,char x,char y) {
-  std::cout << x << "=" << a << "\t" << y << "=" << b << "\n";
+inline void show(int a,int b,char x,char y) {
+  cout << x << "=" << a << "\t" << y << "=" << b << "\n";
 }
 
 int main(void) {
   int    a,b;
-  double c,d;
 
   a = 1; b = 2;
   show(a,b,'a','b');
-  swap(&a,&b);           //int型と判断
+  swap(&a,&b);
+  assert(a == 2 && b == 1);
   show(a,b,'a','b');
-  swap<int>(&a,&b);      //int型を指定
-  show(a,b,'a','b');
-
-  c = 0.5; d = 0.75;
-  std::cout << "\n";
-  show(c,d,'c','d');
-  swap(&c,&d);          //double型と判断
-  show(c,d,'c','d');
-  swap<double>(&c,&d);  //double型を指定
-  show(c,d,'c','d');
-
-
-
 
   return 0;
 }
