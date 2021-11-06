652c0914a19ef05e2600ccdabf606854e0bb404e
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-11-06 20:19:25 +0900

    maxmin

---
+++ maxmin/maxmin2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+#define MAX2(x,y) (x + y + abs(x-y)) / 2
+#define MIN2(x,y) (x + y - abs(x-y)) / 2
+
+int main(void) {
+  int a,b,c;
+
+  a = 3; b = 1; c = 5;
+
+  assert(MAX2(a,b) == a);
+  std::cout << MAX2(a,b)          << "\n";
+  assert(MAX2(c,b) == c);
+  std::cout << MAX2(c,b)          << "\n";
+  assert(MAX2(a,MAX2(b,c)) == c);
+  std::cout << MAX2(a,MAX2(b,c))  << "\n";
+
+  assert(MIN2(a,b) == b);
+  std::cout << MIN2(a,b)          << "\n";
+  assert(MIN2(c,b) == b);
+  std::cout << MIN2(c,b)          << "\n";
+  assert(MIN2(a,MIN2(b,c)) == b);
+  std::cout << MIN2(a,MIN2(b,c))  << "\n";
+}
