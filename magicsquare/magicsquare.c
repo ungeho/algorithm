97efe4362aa4aee61f847be09b827758506abaec
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-11-06 22:07:58 +0900

    magicsquare

--- magicsquare/magicsquare.c
+++ magicsquare/magicsquare.c
@@ -5,29 +5,32 @@
 #define N 4
 
 
-int main(void) {
+void generate_magicsquare(int size,int a[size][size]) {
   int k = 0;
-  int a[N * 2 + 1][N * 2 + 1];
-  int size = N * 2 + 1;
-  //初期化
-  for(int i = 0; i < size; i++) {
-    for(int j = 0; j < size; j++) {
-      a[i][j] = 0;
-    }
-  }
 
-  //生成
   for(int i = -size/2; i <= size/2; i++) {
     for(int j = 0; j < size; j++) {
       a[(j-i+size)%size][(j+i+size)%size] = ++k;
     }
   }
-  //表示
+}
+
+void print_magicsquare(int size,int a[size][size]) {
   for(int i = 0; i < size; i++) {
     for(int j = 0; j < size; j++) {
       printf("%4d",a[i][j]);
     }
     printf("\n");
   }
+}
+
+
+int main(void) {
+  int a[N * 2 + 1][N * 2 + 1];
+  int size = N * 2 + 1;
+
+  generate_magicsquare(size,a);
+  print_magicsquare(size,a);
+
   return 0;
 }
